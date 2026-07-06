7c570ca59c3db1f93b4524be7849431efe0f9f3e
peter88213 <petert88213@gmail.com>
2026-07-06 16:57:23 +0200

    Updated docstring.

--- src/nvcolorpalette/nv_color_chooser.py
+++ src/nvcolorpalette/nv_color_chooser.py
@@ -1,7 +1,7 @@
 """Provide a class to choose a color.
 
 Copyright (c) Peter Triesberger
-For further information see https://github.com/peter88213/
+For further information see https://github.com/peter88213/nv_color_palette
 License: GNU GPLv3 (https://www.gnu.org/licenses/gpl-3.0.en.html)
 """
 
